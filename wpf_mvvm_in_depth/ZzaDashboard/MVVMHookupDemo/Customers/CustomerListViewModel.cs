6ee1d4c6ac247c6883a324d320ab28c20089a681
nf313743 <1612339+nf313743@users.noreply.github.com>
2017-08-08 15:27:18 +0100

    Completed module 3

---
+++ wpf_mvvm_in_depth/ZzaDashboard/MVVMHookupDemo/Customers/CustomerListViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMHookupDemo.Services;
+using Zza.Data;
+
+namespace MVVMHookupDemo.Customers
+{
+    public class CustomerListViewModel
+    {
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        private ICustomersRepository _repo;
+
+        public CustomerListViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                return;
+            }
+            _repo = new CustomersRepository();
+            Customers = new ObservableCollection<Customer>(_repo.GetCustomersAsync().Result);
+        }
+    }
+}
---
+++ wpf_mvvm_in_depth/ZzaDashboard/MVVMHookupDemo/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMHookupDemo.Customers;
+
+namespace MVVMHookupDemo
+{
+    public class MainWindowViewModel
+    {
+        public object CurrentViewModel { get; set; } = new CustomerListViewModel();
+    }
+}
