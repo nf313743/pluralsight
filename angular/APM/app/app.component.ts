378e6add7aeb2c28e7023cbdd83cafce153bb76c
nf313743 <1612339+nf313743@users.noreply.github.com>
2018-01-14 22:04:22 +0000

    Currently not working

--- angular/APM/app/app.component.ts
+++ angular/APM/app/app.component.ts
@@ -1,4 +1,11 @@
+import { Component } from '@angular/core';
 @Component({
+    selector: 'pm-root',
+    template: `
+        <div><h1>{{pageTitle}}</h1>
+            <div>My First Component</div>
+        </div>
+    `
     
 })
 export class AppComponent{
