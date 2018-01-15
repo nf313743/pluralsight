ef8269f43c27ab260262d0a71fc2e9b82d4efdd1
nf313743 <1612339+nf313743@users.noreply.github.com>
2018-01-15 19:15:35 +0000

    Completed module 5

--- angular/APM/app/app.component.js
+++ angular/APM/app/app.component.js
@@ -14,7 +14,7 @@ var AppComponent = /** @class */ (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'pm-root',
-            template: "\n        <div><h1>{{pageTitle}}</h1>\n            <div>My First Component</div>\n        </div>\n    "
+            template: "\n        <div><h1>{{pageTitle}}</h1>\n            <pm-products></pm-products>\n        </div>\n    "
         })
     ], AppComponent);
     return AppComponent;
