bca0ea705bfa61f13b51ffe42a7c7fe0749b09e1
nf313743 <1612339+nf313743@users.noreply.github.com>
2018-01-19 17:40:39 +0000

    Completed module 11

--- angular/APM/src/app/app.module.ts
+++ angular/APM/src/app/app.module.ts
@@ -6,11 +6,31 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConverToSpacesPipe } from './shared/convert-to-spaces-pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router'
+import { WelcomeComponent } from './home/welcome.component';
 
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, HttpClientModule],
-    declarations: [AppComponent, ProductListComponent, ConverToSpacesPipe, StarComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RouterModule.forRoot([
+            { path: 'products', component: ProductListComponent },
+            { path: 'product/:id', component: ProductDetailComponent },
+            { path: 'welcome', component: WelcomeComponent },
+            { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+            { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+        ])
+    ],
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        ConverToSpacesPipe,
+        StarComponent,
+        ProductDetailComponent,
+        WelcomeComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
