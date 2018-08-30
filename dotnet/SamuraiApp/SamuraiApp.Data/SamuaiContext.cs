a759ecde9d02490476cd0a30ac555b14cc824058
nf313743 <1612339+nf313743@users.noreply.github.com>
2018-08-30 18:46:06 +0100

    Completed Module 04

--- dotnet/SamuraiApp/SamuraiApp.Data/SamuaiContext.cs
+++ dotnet/SamuraiApp/SamuraiApp.Data/SamuaiContext.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
     public class SamuaiContext : DbContext
     {
+        public static readonly LoggerFactory MyConsoleLoggerFactory
+            = new LoggerFactory(new[]
+            {
+                new ConsoleLoggerProvider((category, level) =>
+                    category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information, true)
+            });
+
         public DbSet<Samurai> Samurais { get; set; }
 
         public DbSet<Quote> Quotes { get; set; }
@@ -15,7 +25,9 @@ namespace SamuraiApp.Data
         protected override void OnConfiguring(
             DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; " +
+            optionsBuilder
+                .UseLoggerFactory(MyConsoleLoggerFactory)
+                .UseSqlServer("Server = (localdb)\\mssqllocaldb; " +
                 "Database = SamuraiAppData; Trusted_Connection = True; ");
         }
 
--- dotnet/SamuraiApp/SomeUI/Program.cs
+++ dotnet/SamuraiApp/SomeUI/Program.cs
@@ -1,12 +1,96 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
 
 namespace SomeUI
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //InsertSamurai();
+            //InsertMultipleSamurais();
+            SimpleSamuraiQuery();
+            LikeQuery();
+            InsertBattle();
+            UpdateDisconnected();
+            Console.ReadLine();
+        }
+
+        private static void UpdateDisconnected()
+        {
+            using (var context = new SamuaiContext())
+            {
+                var battle = context.Battles.FirstOrDefault();
+                battle.EndDate = new DateTime(1560, 06, 30);
+                using (var context2 = new SamuaiContext())
+                {
+                    context2.Battles.Update(battle);
+                    context2.SaveChanges();
+                }
+            }
+        }
+
+        private static void InsertBattle()
+        {
+            using (var context = new SamuaiContext())
+            {
+                context.Battles.Add(
+                    new Battle
+                    {
+                        Name = "Battle of Okehazama",
+                        StartDate = new DateTime(1560, 5, 1),
+                        EndDate = new DateTime(1560, 06, 15)
+                    });
+                context.SaveChanges();
+            }
+        }
+
+        private static void LikeQuery()
+        {
+            using (var context = new SamuaiContext())
+            {
+                var samurai = context.Samurais
+                    .FirstOrDefault(x => EF.Functions.Like(x.Name, "S%"));
+
+                Console.WriteLine(samurai.Name);
+            }
+        }
+
+        private static void SimpleSamuraiQuery()
+        {
+            using (var context = new SamuaiContext())
+            {
+                var samurais = context.Samurais.ToList();
+
+                foreach (var samurai in samurais)
+                {
+                    Console.WriteLine(samurai.Name);
+                }
+            }
+        }
+
+        private static void InsertMultipleSamurais()
+        {
+            var samurai = new Samurai { Name = "Nathan" };
+            var samurai2 = new Samurai { Name = "Sampson" };
+            using (var context = new SamuaiContext())
+            {
+                context.Samurais.AddRange(samurai, samurai2);
+                context.SaveChanges();
+            }
+        }
+
+        private static void InsertSamurai()
+        {
+            var samurai = new Samurai { Name = "Nathan" };
+            using (var context = new SamuaiContext())
+            {
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
         }
     }
 }
