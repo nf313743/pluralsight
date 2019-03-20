b5d32a6a25a6546933d7717e65c9f39e464f27df
nf313743 <1612339+nf313743@users.noreply.github.com>
2019-03-20 13:52:38 +0000

    Module06 completed

---
+++ angular/Angular-Cli/module03/my-app7/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+const foo = 60;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Hello there!';
+}
