b0a59431148f9736cfe0e2c9cf6453b66406d302
nf313743 <1612339+nf313743@users.noreply.github.com>
2019-03-20 13:49:33 +0000

    Angular To MVC: Module 03 completed.

---
+++ angular/angular_to_mvc/PTC/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace PTC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Product()
+        {
+            return View();
+        }
+    }
+}
