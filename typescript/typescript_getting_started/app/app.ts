4dea75b9c09b5ba7a17344e51679293d01cecb72
Nathan <{ID}+{username}@users.noreply.github.com>
2019-10-05 13:42:41 +0100

    Module 04 completed

--- typescript/typescript_getting_started/app/app.ts
+++ typescript/typescript_getting_started/app/app.ts
@@ -1,13 +1,52 @@
 function startGame() {
-    let playerName:string = 'Audrey';
-    logPlayer(playerName);
+  let playerName: string | undefined = getInputValue("playername");
+  logPlayer(playerName);
 
-    let messageElement = document.getElementById('messages');
-    messageElement!.innerText = 'Welcome to MultiMath! Starting new game...!!';
+  postScore(80,playerName);
+  postScore(-5,playerName);
 }
 
-function logPlayer(name:string){
-    console.log(`New game starting for player: ${name}`);
+function logPlayer(name: string = 'MultiMath Player'): void {
+  console.log(`New game starting for player: ${name}`);
 }
 
-document.getElementById('startGame')!.addEventListener('click', startGame);
+function getInputValue(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById(elementID)
+  );
+
+  if (inputElement.value === "") {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'):void {
+  let logger: (value:string) => void;
+
+  if(score < 0){
+    logger = logError;
+  }
+  else{
+    logger = logMessage;
+  }
+  
+  
+  const scoreElement: HTMLElement | null = document.getElementById(
+    "postedScores"
+  );
+  scoreElement!.innerText = `${score} - ${playerName}`;
+  logger(`Score: ${score}`);
+}
+
+document.getElementById("startGame")!.addEventListener("click", startGame);
+
+const logMessage = (message:string) => console.log(message);
+
+function logError(err:string):void{
+console.error(err);
+}
+
+
+
