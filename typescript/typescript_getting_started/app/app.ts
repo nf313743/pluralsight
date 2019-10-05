b6b6e52a0bb32d7e2e630b32c0d8b0d8301fc440
Nathan <{ID}+{username}@users.noreply.github.com>
2019-10-05 08:54:21 +0100

    Revert "Module 02 completed"
    This reverts commit 50d2484ceb1701917d1901adb21f1397c3bbe1ac.

--- typescript/typescript_getting_started/app/app.ts
+++
@@ -1,7 +0,0 @@
-function startGame() {
-    var messageElement = document.getElementById('messages');
-    messageElement!.innerText = 'Welcome to MultiMath! Starting new game...';
-}
-//hello
-
-document.getElementById('startGame')!.addEventListener('click', startGame);
