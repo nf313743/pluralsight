50d2484ceb1701917d1901adb21f1397c3bbe1ac
Nathan <{ID}+{username}@users.noreply.github.com>
2019-10-05 08:48:03 +0100

    Module 02 completed

---
+++ typescript/typescript_getting_started/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './app/app.ts',
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: false
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { 
+        test: /\.tsx?$/, 
+        use: 'ts-loader',
+        exclude:/node_modules/ }
+    ]
+  }
+};
