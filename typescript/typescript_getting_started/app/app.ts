73c70146dae84f9a99937e35040117bc482a5a51
nf313743 <1612339+nf313743@users.noreply.github.com>
2019-10-05 09:01:28 +0100

    Module 03 completed

--- typescript/typescript_getting_started/app/app.ts
+++ typescript/typescript_getting_started/app/app.ts
@@ -1,7 +1,13 @@
 function startGame() {
-    var messageElement = document.getElementById('messages');
-    messageElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    let playerName:string = 'Audrey';
+    logPlayer(playerName);
+
+    let messageElement = document.getElementById('messages');
+    messageElement!.innerText = 'Welcome to MultiMath! Starting new game...!!';
 }
-//hello
+
+function logPlayer(name:string){
+    console.log(`New game starting for player: ${name}`);
+}
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
