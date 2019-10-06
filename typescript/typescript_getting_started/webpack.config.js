e1914e5d68ac73d4760c6f82ca3363057ab6900d
nf313743 <1612339+nf313743@users.noreply.github.com>
2019-10-06 17:06:53 +0100

    Module 05 completed

--- typescript/typescript_getting_started/webpack.config.js
+++ typescript/typescript_getting_started/webpack.config.js
@@ -1,22 +1,43 @@
 module.exports = {
   entry: './app/app.ts',
   devtool: 'inline-source-map',
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: ['.tsx', '.ts', '.js']
   },
   output: {
     filename: 'bundle.js'
   },
   devServer: {
     inline: false
-  },
-  module: {
-    rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { 
-        test: /\.tsx?$/, 
-        use: 'ts-loader',
-        exclude:/node_modules/ }
-    ]
   }
+};
+
+module.exports = {
+  entry: './js/app.js',
+  // devtool: 'inline-source-map',
+  // resolve: {
+  //   extensions: [ '.tsx', '.ts', '.js' ]
+  // },
+  // output: {
+  //   filename: 'bundle.js'
+  // },
+  devServer: {
+    inline: false
+  }
+  // module: {
+  //   rules: [
+  //     // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+  //     { 
+  //       test: /\.tsx?$/, 
+  //       use: 'ts-loader',
+  //       exclude:/node_modules/ }
+  //   ]
+  // }
 };
