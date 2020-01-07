a4f2924d6ca57b8853f7c16a83a2e02bad4f4bf4
nf313743 <1612339+nf313743@users.noreply.github.com>
2020-01-07 13:46:42 +0000

    Module 4 completed.

--- angular/angular_forms/ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ angular/angular_forms/ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserSettings } from "../data/user-settings";
 
 @Component({
-  selector: 'app-user-settings-form',
-  templateUrl: './user-settings-form.component.html',
-  styleUrls: ['./user-settings-form.component.css']
+  selector: "app-user-settings-form",
+  templateUrl: "./user-settings-form.component.html",
+  styleUrls: ["./user-settings-form.component.css"]
 })
 export class UserSettingsFormComponent implements OnInit {
+  originalUserSettings: UserSettings = {
+    name: "Milton",
+    emailOffers: true,
+    interfaceStyle: "dark",
+    subscriptionType: "Annual",
+    notes: "here are some notes...."
+  };
 
-  constructor() { }
+  userSettings: UserSettings = { ...this.originalUserSettings };
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
