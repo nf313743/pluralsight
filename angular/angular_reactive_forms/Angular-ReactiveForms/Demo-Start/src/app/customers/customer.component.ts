b35206031ba78336659030b51ad0d5a67d726dba
nf313743 <1612339+nf313743@users.noreply.github.com>
2020-01-08 12:33:39 +0000

    Angular Reactive Forms: Module 4 completed

--- angular/angular_reactive_forms/Angular-ReactiveForms/Demo-Start/src/app/customers/customer.component.ts
+++ angular/angular_reactive_forms/Angular-ReactiveForms/Demo-Start/src/app/customers/customer.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
-import { Customer } from './customer';
+import { Component, OnInit } from "@angular/core";
+import { Customer } from "./customer";
+import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  selector: "app-customer",
+  templateUrl: "./customer.component.html",
+  styleUrls: ["./customer.component.css"]
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: "",
+      lastName: "",
+      email: "",
+      sendCatalog: true
+    });
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: "jack",
+      lastName: "Harkness",
+      sendCatalog: false
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log("Saved: " + JSON.stringify(this.customerForm.value));
   }
 }
