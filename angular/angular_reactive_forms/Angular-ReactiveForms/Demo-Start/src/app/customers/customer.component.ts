9375fdd6d4aeb49d38083bf653fd77f97b511a48
nf313743 <1612339+nf313743@users.noreply.github.com>
2020-01-08 14:08:42 +0000

    Angular Reactive Forms: Module 5 completed

--- angular/angular_reactive_forms/Angular-ReactiveForms/Demo-Start/src/app/customers/customer.component.ts
+++ angular/angular_reactive_forms/Angular-ReactiveForms/Demo-Start/src/app/customers/customer.component.ts
@@ -1,6 +1,40 @@
 import { Component, OnInit } from "@angular/core";
 import { Customer } from "./customer";
-import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ValidatorFn
+} from "@angular/forms";
+
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get("email");
+  const confirmControl = c.get("confirmEmail");
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+
+  return { match: true };
+}
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (
+      c.value !== null &&
+      (isNaN(c.value) || c.value < min || c.value > max)
+    ) {
+      return { range: true };
+    }
+    return null;
+  };
+}
 
 @Component({
   selector: "app-customer",
@@ -15,10 +49,19 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit() {
     this.customerForm = this.fb.group({
-      firstName: "",
-      lastName: "",
-      email: "",
-      sendCatalog: true
+      firstName: ["", [Validators.required, Validators.minLength(3)]],
+      lastName: ["", [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group(
+        {
+          email: ["", [Validators.required, Validators.email]],
+          confirmEmail: ["", [Validators.required]]
+        },
+        { validator: emailMatcher }
+      ),
+      sendCatalog: true,
+      phone: "",
+      notification: "email",
+      rating: [null, ratingRange(1, 5)]
     });
   }
 
@@ -33,5 +76,15 @@ export class CustomerComponent implements OnInit {
   save() {
     console.log(this.customerForm);
     console.log("Saved: " + JSON.stringify(this.customerForm.value));
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get("phone");
+    if (notifyVia === "text") {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
