aea4fc8d8d17219288951fbee8d3138fe07137d3
nf313743 <1612339+nf313743@users.noreply.github.com>
2020-08-13 17:26:55 +0100

    Module 04 completed

---
+++ dotnet/razor-pages-getting-started/WiredBrainCoffee/Pages/Contact.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
+
+namespace WiredBrainCoffee.Pages
+{
+    public class ContactModel : PageModel
+    {
+        [BindProperty]
+        public Contact Contact { get; set; }
+
+        public string Message { get; private set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                EmailService.SendEmail(Contact);
+                Message = "Your message has been sent";
+            }
+        }
+
+        public void OnPostSubscribe(string address)
+        {
+            EmailService.SendEmail(address);
+            Message = "You have been added to the mailing list";
+        }
+    }
+}
