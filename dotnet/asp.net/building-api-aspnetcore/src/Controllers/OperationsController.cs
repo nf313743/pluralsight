a49b8bc6b7347322e8f662def571448f833e2988
nf313743 <1612339+nf313743@users.noreply.github.com>
2020-08-18 17:20:14 +0100

    Module 06 completed

---
+++ dotnet/asp.net/building-api-aspnetcore/src/Controllers/OperationsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public OperationsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public ActionResult<bool> ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)_configuration;
+                root.Reload();
+                return Ok();
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
