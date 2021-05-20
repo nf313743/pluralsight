a12ae45c34192e14849383decf3d1e408c089fda
Nathan Flynn <nflynn@czarnikow.com>
2021-05-20 12:58:11 +0100

    Module 07 completed

--- angular/rxjs/APM/src/app/products/product.service.ts
+++ angular/rxjs/APM/src/app/products/product.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, throwError } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { combineLatest, Observable, throwError } from 'rxjs';
+import { catchError, combineAll, map, tap } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
 import { SupplierService } from '../suppliers/supplier.service';
+import {ProductCategoryService} from '../product-categories/product-category.service'
 
 @Injectable({
   providedIn: 'root',
@@ -17,22 +18,30 @@ export class ProductService {
 
   constructor(
     private http: HttpClient,
-    private supplierService: SupplierService
+    private supplierService: SupplierService,
+    private productCategoryService: ProductCategoryService
   ) {}
 
   products$ = this.http.get<Product[]>(this.productsUrl).pipe(
-    map((products) =>
+    tap((data) => console.log('Products: ', JSON.stringify(data))),
+    catchError(this.handleError)
+  );
+
+  productsWithCategory$ = combineLatest([
+    this.products$,
+    this.productCategoryService.productCategories$,
+  ]).pipe(
+    map(([products, categories]) =>
       products.map(
         (product) =>
           ({
             ...product,
             price: product.price * 1.5,
+            category: categories.find((c) => product.categoryId === c.id).name,
             searchKey: [product.productName],
           } as Product)
       )
-    ),
-    tap((data) => console.log('Products: ', JSON.stringify(data))),
-    catchError(this.handleError)
+    )
   );
 
   private fakeProduct(): Product {
