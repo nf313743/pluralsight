1b87392c6dc13cde4a0e64e0ab659ba0fe20867c
Nathan Flynn <nflynn@czarnikow.com>
2021-05-20 11:44:26 +0100

    Module 06 completed

--- angular/rxjs/APM/src/app/products/product.service.ts
+++ angular/rxjs/APM/src/app/products/product.service.ts
@@ -2,29 +2,38 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
 import { SupplierService } from '../suppliers/supplier.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
   private productsUrl = 'api/products';
   private suppliersUrl = this.supplierService.suppliersUrl;
 
-  constructor(private http: HttpClient,
-              private supplierService: SupplierService) { }
+  constructor(
+    private http: HttpClient,
+    private supplierService: SupplierService
+  ) {}
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
-      .pipe(
-        tap(data => console.log('Products: ', JSON.stringify(data))),
-        catchError(this.handleError)
-      );
-  }
+  products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+    map((products) =>
+      products.map(
+        (product) =>
+          ({
+            ...product,
+            price: product.price * 1.5,
+            searchKey: [product.productName],
+          } as Product)
+      )
+    ),
+    tap((data) => console.log('Products: ', JSON.stringify(data))),
+    catchError(this.handleError)
+  );
 
   private fakeProduct(): Product {
     return {
@@ -35,7 +44,7 @@ export class ProductService {
       price: 8.9,
       categoryId: 3,
       // category: 'Toolbox',
-      quantityInStock: 30
+      quantityInStock: 30,
     };
   }
 
@@ -54,5 +63,4 @@ export class ProductService {
     console.error(err);
     return throwError(errorMessage);
   }
-
 }
